b9ed8c9b184aadebdcc7e38ce312331fc17199cd
Alexander Hagen <alexander.hagen@adera.no>
2018-10-23 09:13:00 +0200

    Added Edit functionality.

--- src/components/Guest.js
+++ src/components/Guest.js
@@ -7,15 +7,21 @@ import GuestName from './GuestName';
 
 const Guest = props =>
   <li>
-    <GuestName isEditing={props.isEditing}>{props.name}</GuestName>
+    <GuestName
+      isEditing={props.isEditing}
+      handleNameEdits={e => props.setName(e.target.value)}>
+      {props.name}
+    </GuestName>
     <label>
       <input
         type="checkbox"
         checked={props.isConfirmed}
-        onChange={props.handleConfirmation}
-      /> Confirmed
-              </label>
-    <button onClick={props.handleToggleEditing}>edit</button>
+        onChange={props.handleConfirmation} />
+      Confirmed
+    </label>
+    <button onClick={props.handleToggleEditing}>
+      {props.isEditing ? 'save' : 'edit'}
+    </button>
     <button>remove</button>
   </li>
 
@@ -24,7 +30,8 @@ Guest.PropTypes = {
   isConfirmed: PropTypes.bool.isRequired,
   isEditing: PropTypes.bool.isRequired,
   handleConfirmation: PropTypes.func.isRequired,
-  handleToggleEditing: PropTypes.func.isRequired
+  handleToggleEditing: PropTypes.func.isRequired,
+  setName: PropTypes.func.isRequired
 }
 
 export default Guest
--- src/components/GuestName.js
+++ src/components/GuestName.js
@@ -2,20 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuestName = props => {
-    if (props.isEditing) {
-        return (
-            <input type="text" value={props.children} />
-        )
-    }
+  if (props.isEditing) {
     return (
-        <span>
-            {props.children}
-        </span>
+      <input
+        type="text"
+        value={props.children}
+        onChange={props.handleNameEdits}
+      />
     )
+  }
+  return (
+    <span>
+      {props.children}
+    </span>
+  )
 }
 
 GuestName.PropTypes = {
-    isEditing: PropTypes.bool.isRequired,
+  isEditing: PropTypes.bool.isRequired,
+  handleNameEdits: PropTypes.func.isRequired
 }
 
 export default GuestName
